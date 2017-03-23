be258434d5e904b741b46fbd5ad134fbaa331531
HYC_通讯研发部_李贺威 <lihw@hyc-tec.net>
2017-03-23 21:11:13 +0800

    step 4

--- server.py
+++ server.py
@@ -5,7 +5,42 @@ import sys,io,os
 class ServerException(Exception):
 	#服务器内部错误
 	pass
+class case_no_file(object):
+	#文件不存在，返回True
+	def test(self,handler):
+		return not os.path.exists(handler.full_path)
+	def act(self,handler):
+		#抛出异常，文件未找到
+		raise ServerException('{0} not found'.format(handler.path))
+class case_existing_file(object):
+	#文件存在
+	def test(self,handler):
+		return os.path.isfile(handler.full_path)
+	def act(self,handler):
+		#处理该文件
+		handler.handle_file(handler.full_path)
+class case_always_fail(object):
+	#所有情况都不符合时的默认处理类
+	def test(self,handler):
+		return True
+	def act(self,handler):
+		raise ServerException('Unknown Object {0}'.format(handler.path))
+class case_directory_index_file(object):
+	#浏览器访问根url的时候能返回工作目录下index.html的内容
+	def index_path(self,handler):
+		return os.path.join(handler.full_path,'index.html')
+	#判断目标路径是否是目录 & 目录下是否有index.html
+	def test(self,handler):
+		return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
+	def act(self,handler):
+		#响应index.html
+		handler.handle_file(self.index_path(handler))
 class RequestHandler(BaseHTTPRequestHandler):
+	# 所有可能的情况
+	Cases = [case_no_file,
+			case_existing_file,
+			case_directory_index_file,
+			case_always_fail]
 	#处理请求并返回页面
 	Error_Page = '''\
 		<html>
@@ -14,7 +49,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 				<p>{msg}</p>
 			</body>
 		</html>
-	'''
+	'''	
 	def send_content(self,page,status=200):
 		self.send_response(status)
 		self.send_header("Content-Type", "text/html")
@@ -32,7 +67,6 @@ class RequestHandler(BaseHTTPRequestHandler):
 		try:
 			with open(full_path,'rb') as f:
 				content=f.read()
-			print(content)
 			self.send_content(content)
 		except Exception as msg:
 			print(msg)
@@ -44,23 +78,19 @@ class RequestHandler(BaseHTTPRequestHandler):
 	#处理一个get请求  重写的一个方法
 	def do_GET(self):
 		try:
-			#文件的完整路径
-			#full_path=os.getcwd()+self.path
-			full_path=os.path.join(os.getcwd(),self.path.replace('/',''))
-			#如果该路径不存在
-			if not os.path.exists(full_path):
-				#抛出异常，文件未找到
-				raise ServerException('{0} not found'.format(self.path))
-			#如果该路径是一个文件
-			elif os.path.isfile(full_path):
-				#处理该文件
-				self.handle_file(full_path)
-			else:
-				#如果该路径不是一个文件		
-				raise ServerException('Unknown Object {0}'.format(self.path))
+			#文件的完整路径			
+			self.full_path=os.path.join(os.getcwd(),self.path.replace('/',''))
+			#遍历所有可能的情况
+			for case in self.Cases:
+				#实例化
+				handler=case()
+				#是否符合情况
+				if handler.test(self):
+					#符合情况，进行相应处理
+					handler.act(self)
+					break			
 		#处理异常
 		except Exception as msg:
-			print('存在异常')
 			self.handle_error(msg)
 		
 if __name__=='__main__':

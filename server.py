ff65d3696a36daed48145eed792d92d3c05d9df9
HYC_通讯研发部_李贺威 <lihw@hyc-tec.net>
2017-03-23 16:34:02 +0800

    step 2

--- server.py
+++ server.py
@@ -7,20 +7,44 @@ class RequestHandler(BaseHTTPRequestHandler):
 	Page='''\
 		<html>
 			<body>
-				<p>Hello, web!</p>
+				<table>
+					<tr>  <td>Header</td>         <td>Value</td>          </tr>
+					<tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+					<tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+					<tr>  <td>Client port</td>    <td>{client_port}</td> </tr>
+					<tr>  <td>Command</td>        <td>{command}</td>      </tr>
+					<tr>  <td>Path</td>           <td>{path}</td>         </tr>
+				</table>
 			</body>
 		</html>
 	'''
-	#处理一个get请求  重写的一个方法
-	def do_GET(self):
+	def create_page(self):
+		values={
+			'date_time'   : self.date_time_string(),
+			'client_host' : self.client_address[0],
+			'client_port' : self.client_address[1],
+			'command'     : self.command,
+			'path'        : self.path
+		}
+		#从format参数引入的变量名 类似于自动装填相同key值的数据
+		page=self.Page.format(**values)
+		return page
+
+	def send_content(self,page):
 		self.send_response(200)
 		self.send_header("Content-Type", "text/html")
 		#需要将Page编码成bytes,否则报错
 		#bytes解码会得到str str编码会变成bytes
-		self.send_header("Content-Length", str(len(self.Page)))
+		self.send_header("Content-Length", str(len(page)))
 		#发送一个空白行，表明HTTP头响应结束
 		self.end_headers()
-		self.wfile.write(self.Page.encode('ascii'))
+		self.wfile.write(page.encode('ascii'))
+
+	#处理一个get请求  重写的一个方法
+	def do_GET(self):
+		page=self.create_page()
+		self.send_content(page)
+		
 if __name__=='__main__':
 	serverAddress=('',8080)
 	server=HTTPServer(serverAddress,RequestHandler)

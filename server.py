adb7ac5b58b29b08c8624914aa5eb810fd2cae59
HYC_通讯研发部_李贺威 <lihw@hyc-tec.net>
2017-03-24 16:39:57 +0800

    step 6

--- server.py
+++ server.py
@@ -7,44 +7,66 @@ import subprocess
 class ServerException(Exception):
 	#服务器内部错误
 	pass
-class case_no_file(object):
+class base_case(object):
+	def handle_file(self,handler,full_path):
+		try:
+			with open(full_path,'rb') as f:
+				content=f.read()
+			handler.send_content(content)
+		except Exception as msg:
+			print(msg)
+			msg='{0} cannot be read:{1}'.format(full_path,msg)
+			handler.handle_error(msg)
+	#浏览器访问根url的时候能返回工作目录下index.html的内容
+	def index_path(self,handler):
+		return os.path.join(handler.full_path,'index.html')
+	#要求子类必须实现的接口
+	def test(self,handler):
+		assert False, 'Not implemented.'
+	def act(self,handler):
+		#断言 用来定义抽象函数
+		assert False, 'Not implemented.'
+class case_no_file(base_case):
 	#文件不存在，返回True
 	def test(self,handler):
 		return not os.path.exists(handler.full_path)
 	def act(self,handler):
 		#抛出异常，文件未找到
 		raise ServerException('{0} not found'.format(handler.path))
-class case_existing_file(object):
+class case_existing_file(base_case):
 	#文件存在
 	def test(self,handler):
 		return os.path.isfile(handler.full_path)
 	def act(self,handler):
 		#处理该文件
-		handler.handle_file(handler.full_path)
-class case_always_fail(object):
+		self.handle_file(handler,handler.full_path)
+class case_always_fail(base_case):
 	#所有情况都不符合时的默认处理类
 	def test(self,handler):
 		return True
 	def act(self,handler):
 		raise ServerException('Unknown Object {0}'.format(handler.path))
-class case_directory_index_file(object):
-	#浏览器访问根url的时候能返回工作目录下index.html的内容
-	def index_path(self,handler):
-		return os.path.join(handler.full_path,'index.html')
+class case_directory_index_file(base_case):
 	#判断目标路径是否是目录 & 目录下是否有index.html
 	def test(self,handler):
 		return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
 	def act(self,handler):
 		#响应index.html
-		handler.handle_file(self.index_path(handler))
-class case_cgi_file(object):
+		self.handle_file(handler,self.index_path(handler))
+class case_cgi_file(base_case):
 	#处理脚本文件
+	def run_cgi(self,handler):
+		data=subprocess.check_output(["python", handler.full_path])
+		#父进程等待子进程完成返回子进程向标准输出的输出结果
+		handler.send_content(data)
 	def test(self,handler):
 		return os.path.isfile(handler.full_path) and handler.full_path.endswith('.py')
 	def act(self,handler):
 		#运行脚本文件
-		handler.run_cgi(handler.full_path)
+		self.run_cgi(handler)
 class RequestHandler(BaseHTTPRequestHandler):
+	#请求路径合法则返回相应处理
+	#否则返回错误页面
 	# 所有可能的情况
 	Cases = [case_no_file,
 			case_cgi_file,
@@ -60,10 +82,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 			</body>
 		</html>
 	'''	
-	def run_cgi(self,full_path):
-		data=subprocess.check_output(["python", full_path])
-		#父进程等待子进程完成返回子进程向标准输出的输出结果
-		self.send_content(data)
+	
 	def send_content(self,page,status=200):
 		self.send_response(status)
 		self.send_header("Content-Type", "text/html")
@@ -77,15 +96,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 		if isinstance(page,str):
 			page=page.encode('ascii')
 		self.wfile.write(page)
-	def handle_file(self,full_path):
-		try:
-			with open(full_path,'rb') as f:
-				content=f.read()
-			self.send_content(content)
-		except Exception as msg:
-			print(msg)
-			msg='{0} cannot be read:{1}'.format(self.path,msg)
-			self.handle_error(msg)
+	
 	def handle_error(self,msg):
 		content=self.Error_Page.format(path=self.path,msg=msg)
 		self.send_content(content,404)
